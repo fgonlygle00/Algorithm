89321441340cfc747f99c9e14f4d3d7ad018b57b
fgonlygle00 <154571034+fgonlygle00@users.noreply.github.com>
2024-01-29 09:07:39 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.19 ms, Memory: 30.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cs
@@ -0,0 +1,11 @@
+public class Solution 
+{
+    public string solution(string s) 
+    {
+        string answer = "";
+        int mid = s.Length / 2;
+        if (s.Length % 2 == 0) answer = s.Substring(mid - 1, 2);
+        else answer = s.Substring(mid, 1);
+        return answer;
+    }
+}

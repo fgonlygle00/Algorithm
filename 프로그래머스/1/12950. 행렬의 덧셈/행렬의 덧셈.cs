e6bfbc8dc11aafbf332cd41530fb741d82f51b23
fgonlygle00 <154571034+fgonlygle00@users.noreply.github.com>
2024-02-08 09:04:26 +0900

    [level 1] Title: 행렬의 덧셈, Time: 2.23 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.cs
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.cs
@@ -2,12 +2,12 @@ public class Solution
 {
     public int[,] solution(int[,] arr1, int[,] arr2) 
     {
-        int rowCount = arr1.GetLength(0);
-        int colCount = arr1.GetLength(1);
-        int[,] answer = new int[rowCount, colCount];
-        for (int i = 0; i < rowCount; i++) 
+        int x = arr1.GetLength(0);
+        int y = arr1.GetLength(1);
+        int[,] answer = new int[x, y];
+        for (int i = 0; i < x; i++) 
         {
-            for (int j = 0; j < colCount; j++) 
+            for (int j = 0; j < y; j++) 
             {
                 answer[i, j] = arr1[i, j] + arr2[i, j];
             }
